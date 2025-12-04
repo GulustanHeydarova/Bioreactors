3f07e92338b06e786f5c89b0a3d15806538b4d5c
Gulustan Heydarova <heydrovagulustan@gmail.com>
2025-12-04 16:18:13 +0400

    Update and rename 47-2.py to Fermentation/47-2.py

--- Fermentation/47-2.py
+++ Fermentation/47-2.py
@@ -1,7 +1,3 @@
-# File 47-2.py
-# Example 47. Fermentation - Monod model
-# Copyright (C) 2016, Szczepan Bednarz
-# Released under the GNU General Public License
 
 import matplotlib.pyplot as plt
 import numpy as np
